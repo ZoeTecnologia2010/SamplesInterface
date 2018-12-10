4f028f2d1147d8aa0e1d2aef5b0f43f96979c8c1
Zoe Tecnologia <zoetecnologia@hotmail.com>
2018-12-10 09:48:25 -0400

    Initial commit

---
+++ SampleInterface/FormMain.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SampleInterface
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonOrder = new System.Windows.Forms.Button();
+            this.buttonInvoice = new System.Windows.Forms.Button();
+            this.buttonDelivery = new System.Windows.Forms.Button();
+            this.buttonStock = new System.Windows.Forms.Button();
+            this.buttonCustomer = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonSample = new System.Windows.Forms.Button();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelTopLine = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Black;
+            this.panelMenu.Controls.Add(this.buttonExit);
+            this.panelMenu.Controls.Add(this.buttonOrder);
+            this.panelMenu.Controls.Add(this.buttonInvoice);
+            this.panelMenu.Controls.Add(this.buttonDelivery);
+            this.panelMenu.Controls.Add(this.buttonStock);
+            this.panelMenu.Controls.Add(this.buttonCustomer);
+            this.panelMenu.Controls.Add(this.buttonHome);
+            this.panelMenu.Controls.Add(this.buttonSample);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 600);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.ForeColor = System.Drawing.Color.White;
+            this.buttonExit.Location = new System.Drawing.Point(0, 434);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(200, 62);
+            this.buttonExit.TabIndex = 8;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonOrder
+            // 
+            this.buttonOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonOrder.FlatAppearance.BorderSize = 0;
+            this.buttonOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOrder.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOrder.ForeColor = System.Drawing.Color.White;
+            this.buttonOrder.Location = new System.Drawing.Point(0, 372);
+            this.buttonOrder.Name = "buttonOrder";
+            this.buttonOrder.Size = new System.Drawing.Size(200, 62);
+            this.buttonOrder.TabIndex = 7;
+            this.buttonOrder.Text = "Order";
+            this.buttonOrder.UseVisualStyleBackColor = true;
+            // 
+            // buttonInvoice
+            // 
+            this.buttonInvoice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonInvoice.FlatAppearance.BorderSize = 0;
+            this.buttonInvoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInvoice.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInvoice.ForeColor = System.Drawing.Color.White;
+            this.buttonInvoice.Location = new System.Drawing.Point(0, 310);
+            this.buttonInvoice.Name = "buttonInvoice";
+            this.buttonInvoice.Size = new System.Drawing.Size(200, 62);
+            this.buttonInvoice.TabIndex = 6;
+            this.buttonInvoice.Text = "Invoice";
+            this.buttonInvoice.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelivery
+            // 
+            this.buttonDelivery.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDelivery.FlatAppearance.BorderSize = 0;
+            this.buttonDelivery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDelivery.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelivery.ForeColor = System.Drawing.Color.White;
+            this.buttonDelivery.Location = new System.Drawing.Point(0, 248);
+            this.buttonDelivery.Name = "buttonDelivery";
+            this.buttonDelivery.Size = new System.Drawing.Size(200, 62);
+            this.buttonDelivery.TabIndex = 5;
+            this.buttonDelivery.Text = "Delivery";
+            this.buttonDelivery.UseVisualStyleBackColor = true;
+            // 
+            // buttonStock
+            // 
+            this.buttonStock.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonStock.FlatAppearance.BorderSize = 0;
+            this.buttonStock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStock.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStock.ForeColor = System.Drawing.Color.White;
+            this.buttonStock.Location = new System.Drawing.Point(0, 186);
+            this.buttonStock.Name = "buttonStock";
+            this.buttonStock.Size = new System.Drawing.Size(200, 62);
+            this.buttonStock.TabIndex = 4;
+            this.buttonStock.Text = "Stock";
+            this.buttonStock.UseVisualStyleBackColor = true;
+            // 
+            // buttonCustomer
+            // 
+            this.buttonCustomer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonCustomer.FlatAppearance.BorderSize = 0;
+            this.buttonCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCustomer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCustomer.ForeColor = System.Drawing.Color.White;
+            this.buttonCustomer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonCustomer.Location = new System.Drawing.Point(0, 124);
+            this.buttonCustomer.Name = "buttonCustomer";
+            this.buttonCustomer.Size = new System.Drawing.Size(200, 62);
+            this.buttonCustomer.TabIndex = 3;
+            this.buttonCustomer.Text = "Customer";
+            this.buttonCustomer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonCustomer.UseVisualStyleBackColor = true;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonHome.FlatAppearance.BorderSize = 0;
+            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHome.ForeColor = System.Drawing.Color.White;
+            this.buttonHome.Location = new System.Drawing.Point(0, 62);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(200, 62);
+            this.buttonHome.TabIndex = 2;
+            this.buttonHome.Text = "Home";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            // 
+            // buttonSample
+            // 
+            this.buttonSample.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSample.Enabled = false;
+            this.buttonSample.FlatAppearance.BorderSize = 0;
+            this.buttonSample.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSample.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSample.ForeColor = System.Drawing.Color.White;
+            this.buttonSample.Location = new System.Drawing.Point(0, 0);
+            this.buttonSample.Name = "buttonSample";
+            this.buttonSample.Size = new System.Drawing.Size(200, 62);
+            this.buttonSample.TabIndex = 1;
+            this.buttonSample.UseVisualStyleBackColor = true;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.panelControls);
+            this.panelMain.Controls.Add(this.panelTop);
+            this.panelMain.Controls.Add(this.panelTopLine);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(200, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(556, 600);
+            this.panelMain.TabIndex = 1;
+            // 
+            // panelControls
+            // 
+            this.panelControls.BackColor = System.Drawing.Color.Black;
+            this.panelControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControls.Location = new System.Drawing.Point(0, 106);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(556, 494);
+            this.panelControls.TabIndex = 2;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.Black;
+            this.panelTop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelTop.BackgroundImage")));
+            this.panelTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 12);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(556, 94);
+            this.panelTop.TabIndex = 1;
+            // 
+            // panelTopLine
+            // 
+            this.panelTopLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panelTopLine.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopLine.Location = new System.Drawing.Point(0, 0);
+            this.panelTopLine.Name = "panelTopLine";
+            this.panelTopLine.Size = new System.Drawing.Size(556, 12);
+            this.panelTopLine.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 600);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sample Interface";
+            this.panelMenu.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelControls;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelTopLine;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonOrder;
+        private System.Windows.Forms.Button buttonInvoice;
+        private System.Windows.Forms.Button buttonDelivery;
+        private System.Windows.Forms.Button buttonStock;
+        private System.Windows.Forms.Button buttonCustomer;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonSample;
+    }
+}
+
